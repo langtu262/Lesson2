a75f7734f9e550b84d9cba18f9a291617174abba
langtu262 <trungkienit87@gmail.com>
2024-03-12 23:02:36 +0700

    lesson6

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float damaged;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Damage")
+        {
+            damaged = +5;
+            Destroy(gameObject);
+            Debug.Log(damaged);
+        }
+    }
+}
