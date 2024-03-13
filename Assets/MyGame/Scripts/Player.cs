c8344adaaa0105f066636895ab841649da1f702c
langtu262 <trungkienit87@gmail.com>
2024-03-13 16:02:53 +0700

    2024

--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -6,23 +6,47 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     private float damaged;
+    public float fuel;
+    private float capacity = 100f;
     void Start()
     {
-        
+        fuel = capacity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Damage")
         {
-            damaged = +5;
+            damaged += 5;
             Destroy(collision.gameObject);
             Debug.Log(damaged);
         }
+        if (collision.gameObject.name == "BarrelRed")
+        {
+            fuel +=25;
+            Destroy(collision.gameObject);
+            if (fuel >= 100)
+            {
+                fuel = 100;
+            }
+            Debug.Log(fuel);
+        }
+        if (collision.gameObject.name == "BarrelBlue")
+        {
+            capacity += 5;
+            Destroy(collision.gameObject);
+            if (capacity >= 100)
+            {
+                capacity = 100;
+            }
+            Debug.Log(fuel);
+        }
     }
+
+
 }
